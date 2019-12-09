98f4d7e05bb49c92cb6552302f097bfc06948f2b
Eric Ferguson <turtle0888@gmail.com>
2019-12-09 16:27:54 -0600

    Add cart context API

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import data from './data';
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
-import { ProductContext } from './contexts/ProductContext'
-
+import { ProductContext } from './contexts/ProductContext';
+import { CartContext } from './contexts/CartContext';
 
 function App() {
 	const [products] = useState(data);
@@ -20,20 +20,20 @@ function App() {
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
-
-			{/* Routes */}
 			<ProductContext.Provider value={{ products, addItem}}>
-			<Route
-				exact
-				path="/"
-				component={Products} 
-			/>
+			<CartContext.Provider value={{ cart }}>
+				<Navigation/>
+				<Route
+					exact
+					path="/"
+					component={Products} 
+				/>
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
+				<Route
+					path="/cart"
+					component= {ShoppingCart} />
+				/>
+			</CartContext.Provider>
 			</ProductContext.Provider>
 		</div>
 	);
